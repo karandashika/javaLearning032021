395b4bff4211dde9ea72c2f5bb6761975b7276ce
karandashika <Programmistka1>
2021-04-08 21:46:18 +0700

    add files

---
+++ src/java/ru/synergyitacademy/lesson1/Launcher.java
@@ -0,0 +1,15 @@
+package ru.synergyitacademy.lesson1;
+
+public class Launcher {
+
+    /**
+     * Основная точка входа в приложение
+     *
+     * @param args список аргументов, которые можно кастомизировать
+     * @throws Exception список исключений, которые генерируются во время выполнения программы
+     */
+    public static void main(String[] args) throws Exception {
+        String str = "Привет, мир!";
+        System.out.println(str);
+    }
+}
